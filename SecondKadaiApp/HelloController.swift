9e3c62a5474d463be8ee7f98a0eaea791bb03059
shino8_0921 <ntm10107@gmail.com>
2021-10-05 22:07:24 +0900

    プログラムを作成しました。

---
+++ SecondKadaiApp/HelloController.swift
@@ -0,0 +1,36 @@
+//
+//  HelloController.swift
+//  SecondKadaiApp
+//
+//  Created by 吉田なつみ on 2021/09/24.
+//
+
+import UIKit
+
+class HelloController: UIViewController {
+  
+    @IBAction func back(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    var moji = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         NameLabel.text = "こんにちは \(moji) さん"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var LabelName: UILabel!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // segueから遷移先のResultViewControllerを取得する
+            let HelloController:HelloController = segue.destination as! HelloController
+            // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+            HelloController.moji = TextField.text!
+    }
+    @IBAction func unwindPrev(_ segue: UIStoryboardSegue) {
+    }
 
+    
+    @IBAction func handle(_ sender: Any) {
+    }
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        LabelName.text = "名前を入力してください"
         // Do any additional setup after loading the view.
     }
+   
 
 
 }
